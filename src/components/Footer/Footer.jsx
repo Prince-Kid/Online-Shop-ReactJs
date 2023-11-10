465d26f3e440e4d6cd0b53b4d5708181650338a5
Prince-kid <mucyoprinc12@gmail.com>
2023-11-10 17:23:09 +0200

    comit1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -47,22 +47,22 @@ const Footer = () => {
               <li>Phone: +250783154587</li>
             </ul>
             <div className="socialMedia links">
+                <a href="https://wa.link/v7lbdi" className='linkedIn' style={{"--color": "#48C857"}}>
+                <i class="fa-brands fa-whatsapp"></i></a>
                     <a href="https://www.linkedin.com/in/mucyo-prince-29321421b/" className='linkedIn' style={{"--color": "#0072b1"}}>
                         <i className='fa-brands fa-linkedin-in'></i>
                     </a>
-                    <a href="https://twitter.com/mucyoprince12" className='instagram' style={{"--color":"#E1306c"}}>
+                    <a href="https://www.instagram.com/mucyoprince12/" className='instagram' style={{"--color":"#E1306c"}}>
                         <i className='fa-brands fa-instagram'></i>
                     </a>
-                    <a href="https://twitter.com/mucyoprince12" className='facebook' style={{"--color":"#4267B2"}}>
+                    <a href="https://web.facebook.com/johnx.prince" className='facebook' style={{"--color":"#4267B2"}}>
                         <i className='fa-brands fa-facebook-f'></i>
                     </a>
                     <a href="https://twitter.com/mucyoprince12" className='twitter' style={{"--color":"#01C2F7"}}>
                         <i className='fa-brands fa-twitter'></i>
                     </a>
-                    <a href="https://twitter.com/mucyoprince12" className='tiktok' style={{"--color":"#FF0050"}}>
-                        <i className='fa-brands fa-tiktok'></i>
-                    </a>
-                    <a href="https://twitter.com/mucyoprince12" className='github'style={{"--color" :"white"}}>
+                    
+                    <a href="https://github.com/Prince-Kid" className='github'style={{"--color" :"white"}}>
                         <i className='fa-brands fa-github'></i>
                     </a>
                 </div>
